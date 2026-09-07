f2a53100e6b6fbf0ccae2984041a91581463750a
MyBuddyMaid Admin <admin@mybuddymaid.com>
2026-09-07 15:57:22 +0530

    chore: Securely move Supabase credentials to .env variables

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://xrjtisqlvksbhpdkpyde.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhyanRpc3FsdmtzYmhwZGtweWRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg3NzA2MzksImV4cCI6MjEwNDM0NjYzOX0.sgO1Yvq7mECv-_COBJKzZM0fErwPhueo0a8sIq8LS1w';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: window.localStorage,
